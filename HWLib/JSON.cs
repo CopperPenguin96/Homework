71aeff0a6ef6e9414bfeaa51f657f13fd97331c6
Alex Potter <alexe364981@gmail.com>
2014-09-25 08:58:52 -0400

    Library Writes Course JSON's
    Library can now write to the Course JSON files in the AppData folder

--- HWLib/JSON.cs
+++ HWLib/JSON.cs
@@ -4,17 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace HWLib
 {
     public class JSON
     {
+        /// <summary>
+        /// Writes all the JSON files based on the User's classes
+        /// </summary>
+        /// <param name="classes">UserInfo.userClasses()</param>
         public static void writeClassesJSON(Class[] classes)
         {
-            
-            for (int classCount = 1; classCount <= classes.Length; classCount++)
+            String[] allJSONStrings = new String[classes.Length];
+            int loopCount = 1;
+            foreach (Class cc in classes)
             {
-                
+                String jsonString = JsonConvert.SerializeObject(cc);
+                allJSONStrings[loopCount] = jsonString;
+                loopCount++;
+            }
+            loopCount = 1;
+            foreach (String arrayJSON in allJSONStrings)
+            {
+                File.WriteAllText(Constants.courseFiles()[loopCount], arrayJSON);
+                loopCount++;
             }
         }
     }
