f23433f8aabcab994a62fbd2f86efbca8c8be716
Alex Potter <alexe364981@gmail.com>
2014-09-25 09:30:36 -0400

    Fixes and Work

--- HWLib/User/UserInfo.cs
+++ HWLib/User/UserInfo.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HWLib
+namespace HWLib.User
 {
-    class UserInfo
+    public class UserInfo
     {
         public String userName()
         {
