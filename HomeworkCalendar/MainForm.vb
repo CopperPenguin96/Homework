62fda3db7a192172a7047fe5564d028340a62538
Alex Potter <alexe364981@gmail.com>
2014-10-14 09:24:35 -0400

    GitIgnore

--- HomeworkCalendar/MainForm.vb
+++ HomeworkCalendar/MainForm.vb
@@ -1,12 +1,13 @@
-﻿Public Class MainForm
+﻿Imports HWLib.Objects
+Imports HWLib.JSON
+
+Public Class MainForm
 
     Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         UserInfoForm.Close()
         CourseForm.Close()
+        LoadContent()
     End Sub
-
-    
- 
     Private Sub btnNew_Click(sender As Object, e As EventArgs) Handles btnNew.Click
         Dim newType As TypeSelection = New TypeSelection(HWLib.Objects.Assignments.Action.AddNew)
         newType.ShowDialog()
@@ -20,5 +21,10 @@
     Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
         Dim newType As TypeSelection = New TypeSelection(HWLib.Objects.Assignments.Action.Delete)
         newType.ShowDialog()
+    End Sub
+    Sub LoadContent()
+        For Each classes As Course In CourseReader.courses
+            lboClasses.Items.Add(classes)
+        Next
     End Sub
 End Class
